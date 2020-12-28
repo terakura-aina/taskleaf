578dc9434ba337cf81a9e439acc394cb38078aae
terakura aina <aicaierwei@gmail.com>
2020-12-28 11:29:42 +0900

    マイグレーションファイルにNOT NULL制約追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_080735) do
+ActiveRecord::Schema.define(version: 2020_12_28_015508) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
