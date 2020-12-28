cbbbe7e7ef174b859344bbb454aa360a3da4e9b6
terakura aina <aicaierwei@gmail.com>
2020-12-28 12:15:06 +0900

    バリデーション追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+  #自作の場合はvalidate、用意されているバリデーションはvalidates
+
+  private
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
