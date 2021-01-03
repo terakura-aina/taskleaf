a642b4fb09106ff825b6cfcc443a858d66f9e8fd
terakura aina <aicaierwei@gmail.com>
2021-01-03 16:20:10 +0900

    adminのみ機能追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,8 @@ class Task < ApplicationRecord
   validate :validate_name_not_including_comma
   #自作の場合はvalidate、用意されているバリデーションはvalidates
 
+  belongs_to :user
+
   private
 
   def validate_name_not_including_comma
