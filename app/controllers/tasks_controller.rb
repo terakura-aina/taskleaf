2e93955f45c10aa0251fbe240ee976340164f75c
terakura aina <aicaierwei@gmail.com>
2021-01-04 11:29:33 +0900

    chapter4 完了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = current_user.tasks.order(created_at: :desc)
   end
 
-  def show
-    @task = current_user.tasks.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -20,19 +19,15 @@ class TasksController < ApplicationController
     end
   end
 
-  def edit
-    @task = current_user.tasks.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = current_user.tasks.find(params[:id])
     @task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
   end
 
   def destroy
-    task = Task.find(params[:id])
-    task.destroy
+    @task.destroy
     redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
 
@@ -40,5 +35,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
